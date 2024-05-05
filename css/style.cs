a657dcc9d3e32598a41eb5aec777fe0ac1b8d002
FranV <fvcanquil@gmail.com>
2024-05-05 18:33:48 -0400

    agregando variables y mixin

---
+++ css/style.cs
@@ -0,0 +1,17 @@
+/* Error: expected ";".
+ *    ,
+ * 13 |   h1 {
+ *    |      ^
+ *    '
+ *   SCSS\styles.scss 13:6  root stylesheet */
+
+body::before {
+  font-family: "Source Code Pro", "SF Mono", Monaco, Inconsolata, "Fira Mono",
+      "Droid Sans Mono", monospace, monospace;
+  white-space: pre;
+  display: block;
+  padding: 1em;
+  margin-bottom: 1em;
+  border-bottom: 2px solid black;
+  content: 'Error: expected ";".\a    \2577 \a 13 \2502    h1 {\d\a    \2502       ^\a    \2575 \a   SCSS\\styles.scss 13:6  root stylesheet';
+}
